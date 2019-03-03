722ea6efb16abc620b07744823bd855015e4c172
Shearer <alexshearer00@gmail.com>
2019-03-03 22:03:59 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         echo360 speed mode
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Adds higher speed options to echo360 lcs
+// @author       Alexander Shearer
+// @match        https://echo360.org.au/*
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("adding memes");
+    $('div.video-menu.settings-menu').bind("DOMSubtreeModified", function settingsHandler(e){
+        $('div.video-menu.settings-menu').unbind("DOMSubtreeModified", settingsHandler);
+        $('#speed-select').append(new Option("2.25", 2.25));
+        $('#speed-select').append(new Option("2.5", 2.5));
+        $('#speed-select').append(new Option("2.75", 2.75));
+        $('#speed-select').append(new Option("3", 3));
+        $('div.video-menu.settings-menu').bind("DOMSubtreeModified", settingsHandler);
+    } );
+})();
