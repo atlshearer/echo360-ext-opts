ba0e6a1d88bb7d5c2bd0c5887450b507bf59830c
Shearer <alexshearer00@gmail.com>
2020-05-22 23:04:21 +0800

    Update script.js
    Fixed issue where page would load too quickly

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         echo360 speed mode
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Adds higher speed options to echo360 lcs
 // @author       Alexander Shearer
 // @match        https://echo360.org.au/*
@@ -12,12 +12,17 @@
 (function() {
     'use strict';
     console.log("adding memes");
-    $('div.video-menu.settings-menu').bind("DOMSubtreeModified", function settingsHandler(e){
-        $('div.video-menu.settings-menu').unbind("DOMSubtreeModified", settingsHandler);
-        $('#speed-select').append(new Option("2.25", 2.25));
-        $('#speed-select').append(new Option("2.5", 2.5));
-        $('#speed-select').append(new Option("2.75", 2.75));
-        $('#speed-select').append(new Option("3", 3));
-        $('div.video-menu.settings-menu').bind("DOMSubtreeModified", settingsHandler);
-    } );
+    window.addEventListener('load', function() {
+        $('div.video-menu.settings-menu').bind("DOMSubtreeModified", function settingsHandler(e){
+            console.log("Memes added...");
+            $('div.video-menu.settings-menu').unbind("DOMSubtreeModified", settingsHandler);
+            $('#speed-select').append(new Option("2.25", 2.25));
+            $('#speed-select').append(new Option("2.5", 2.5));
+            $('#speed-select').append(new Option("2.75", 2.75));
+            $('#speed-select').append(new Option("3", 3));
+            $('div.video-menu.settings-menu').bind("DOMSubtreeModified", settingsHandler);
+        } );
+        console.log("Memes added");
+    }, false);
+
 })();
